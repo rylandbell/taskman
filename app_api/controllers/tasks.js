7ea7ea50b8148a2dbedbc6362e797b45fc6eca32
Ryland Bell <ryland@Rylands-MacBook-Air.local>
2016-04-12 10:48:59 -0700

    just beginning to set up API

---
+++ app_api/controllers/tasks.js
@@ -0,0 +1,6 @@
+module.exports.tasksListByDateAdded = function (req, res) {};
+module.exports.tasksReadOne = function (req, res) {};
+module.exports.tasksCreate = function (req, res) {};
+module.exports.tasksUpdateOne = function (req, res) {};
+module.exports.tasksDeleteOne = function (req, res) {};
+
---
+++ app_api/routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var ctrlTasks = require('../controllers/tasks');
+
+// routes for calls to tasks folder:
+router.get('/tasks', ctrlTasks.tasksListByDateAdded);
+router.get('/tasks/:taskid', ctrlTasks.tasksReadOne);
+router.post('/tasks', ctrlTasks.tasksCreate);
+router.put('/tasks/:taskid', ctrlTasks.tasksUpdateOne);
+router.delete('/tasks/:taskid', ctrlTasks.tasksDeleteOne);
+
+module.exports = router;
