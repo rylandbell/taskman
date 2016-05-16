81f93e495ae6092dfaca569f02f3800110d46805
Ryland Bell <ryland@internalcheck.apple.com>
2016-05-15 21:05:07 -0700

    send to google works, UI flow still poor

--- public/javascripts/calendar.js
+++ public/javascripts/calendar.js
@@ -1,58 +1,56 @@
-var goog = talkToGoogleApi();
-
-//All communication with Google servers should be in this module:
-function talkToGoogleApi(){
-  var exports = {};
-  // Your Client ID can be retrieved from your project in the Google
-  // Developer Console, https://console.developers.google.com
-  var CLIENT_ID = '634607653489-3duvkicl6nstmfktt3v0f1ecqbi34e54.apps.googleusercontent.com';
-  var SCOPES = ["https://www.googleapis.com/auth/calendar"];
-
-  // Ask server if app is authorized to modify calendar
-  exports.checkAuth = function(immediate) {
-    var context = this;
-    gapi.auth.authorize(
-      {
-        'client_id': CLIENT_ID,
-        'scope': SCOPES.join(' '),
-        'immediate': immediate
-      }, function(token){
-        context.handleAuthResult.apply(context,[token]);
-      });
-  };
-
-  // Handle response from authorization server
-  exports.handleAuthResult = function(authResult) {
-    if (authResult && !authResult.error) {
-      this.loadCalendarApi();
-      updateAuthDisplay(true);
-    } else {
-      updateAuthDisplay(false);
-    }
-  };
-
-  //Load Google Calendar client library.
-  exports.loadCalendarApi = function() {
-    gapi.client.load('calendar', 'v3');
-  };
-
-  //Add event to calendar
-  exports.addEvent = function(event){
-    var request = gapi.client.calendar.events.insert({
-      'calendarId': 'primary',
-      'resource': event
-    });
-
-    request.execute(function(event) {
-      // appendPre('Event created: ' + event.htmlLink);
-    });
-  }
-
-  return exports;
-}
-
 // All DOM manipulation and event listeners go here:
 $('document').ready(function(){
+  var goog = talkToGoogleApi();
+  //All communication with Google servers should be in this module:
+  function talkToGoogleApi(){
+    var exports = {};
+    // Your Client ID can be retrieved from your project in the Google
+    // Developer Console, https://console.developers.google.com
+    var CLIENT_ID = '634607653489-3duvkicl6nstmfktt3v0f1ecqbi34e54.apps.googleusercontent.com';
+    var SCOPES = ["https://www.googleapis.com/auth/calendar"];
+
+    // Ask server if app is authorized to modify calendar
+    exports.checkAuth = function(immediate) {
+      var context = this;
+      gapi.auth.authorize(
+        {
+          'client_id': CLIENT_ID,
+          'scope': SCOPES.join(' '),
+          'immediate': immediate
+        }, function(token){
+          context.handleAuthResult.apply(context,[token]);
+        });
+    };
+
+    // Handle response from authorization server
+    exports.handleAuthResult = function(authResult) {
+      if (authResult && !authResult.error) {
+        this.loadCalendarApi();
+        updateAuthDisplay(true);
+      } else {
+        updateAuthDisplay(false);
+      }
+    };
+
+    //Load Google Calendar client library.
+    exports.loadCalendarApi = function() {
+      gapi.client.load('calendar', 'v3');
+    };
+
+    //Add event to calendar
+    exports.addEvent = function(event){
+      var request = gapi.client.calendar.events.insert({
+        'calendarId': 'primary',
+        'resource': event
+      });
+      request.execute(function(event) {
+        console.log('holla back? '+event.htmlLink);
+        // appendPre('Event created: ' + event.htmlLink);
+      });
+    };
+    return exports;
+  }
+
   //Show appropriate UI elements, depending on authorized status
   function updateAuthDisplay(authorized){
     var authorizePanel = document.getElementById('auth-panel');
@@ -66,29 +64,44 @@ $('document').ready(function(){
     }
   }
 
-  $('#cal-init').on('click',function(){
-    goog.checkAuth(false);
-  });
-
-  $('#send-btn').on('click',function(e){
-    e.preventDefault();
-    // get info from form, assemble into event object:
-    // var newEvent = 
-    goog.addEvent(newEvent);
-  });
-
-  var sampleEvent = {
-    'summary': 'Google I/O 2015',
-    'description': 'A chance to hear more about Google\'s developer products.',
-    'start': {
-      'dateTime': '2016-05-28T09:00:00-07:00',
-      'timeZone': 'America/Los_Angeles'
-    },
-    'end': {
-      'dateTime': '2016-05-28T17:00:00-07:00',
-      'timeZone': 'America/Los_Angeles'
+  function prepEventObject(){
+    // Build a data object from the event form:
+    var formDataObject = {}, newEvent = {};
+    var $form = $('#calendar-form');
+    var formDataArray = $form.serializeArray();
+    for (var i = 0; i<formDataArray.length; i++){
+      formDataObject[formDataArray[i].name]=formDataArray[i].value;
     }
-  };
+
+    // Translate the form data into the event format to be sent to Google
+    newEvent.summary = formDataObject.name;
+    newEvent.start = {
+      timeZone: 'America/Los_Angeles',
+      dateTime: setTimes(formDataObject.date,0) 
+    };
+    newEvent.end = {
+      timeZone: 'America/Los_Angeles',
+      dateTime: setTimes(formDataObject.date,formDataObject.duration)
+    };
+    function setTimes(date,duration){
+      var targetTime = new Date(date);
+      targetTime.setMinutes(targetTime.getMinutes()+duration);
+      return targetTime.toISOString();
+    }
+    return newEvent;
+  }
+
+  //check authorization when user initiates calendar add process
+  $('#cal-init').on('click',function(){
+    goog.checkAuth(true);
+  });
+
+  //send data to google calendar when user submits calendar form
+  $('#send-btn').on('click',function(e){
+    e.preventDefault();   
+    var myEvent = prepEventObject();
+    goog.addEvent(myEvent);
+  });
 });
 
 
