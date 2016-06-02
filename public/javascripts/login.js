2648ab86d59274377cea335e0ed607d8ca2b0ab8
Ryland Bell <ryland.bell@gmail.com>
2016-06-02 10:34:49 -0700

    Login, register forms correctly prevent submission with blank fields

---
+++ public/javascripts/login.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	function checkForBlanks(targetForm,e){
+		targetForm
+			.find(':input')
+			.not(':button')
+			.each(function(){
+				if(!$(this).val()){
+					e.preventDefault();
+					$('#message-box')
+						.text('All fields are required.')
+						.removeClass('invisible');
+				}
+			});
+	}
+
+	$('form').on('submit',function(e){
+		checkForBlanks($(this),e);
+	});
+});
