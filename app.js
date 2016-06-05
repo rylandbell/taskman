91c9809ee24b722ba5719e252553456f270c18e1
Ryland Bell <ryland.bell@gmail.com>
2016-06-05 11:08:02 -0700

    adds scss support

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var passport = require('passport');
+var sassMiddleware = require('node-sass-middleware');
 
 require('./app_api/models/db');
 require('./app_api/config/passport');
@@ -18,6 +19,16 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'jade');
+
+// SASS middleware must be before express.static
+app.use(sassMiddleware({
+    /* Options */
+    src: path.join(__dirname, '/scss'),
+    dest: path.join(__dirname, 'public/stylesheets'),
+    debug: true,
+    outputStyle: 'compressed',
+    prefix:  '/stylesheets'  // Where prefix is at <link rel="stylesheets" href="prefix/style.css"/>
+}));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
