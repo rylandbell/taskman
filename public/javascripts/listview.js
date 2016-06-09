52968c54121bf95adf6dd1c607734b2b86573baa
Ryland Bell <ryland.bell@gmail.com>
2016-06-09 13:53:22 -0700

    fixes link-to-details on new tasks, refines style

--- public/javascripts/listview.js
+++ public/javascripts/listview.js
@@ -13,7 +13,7 @@ $('document').ready(function () {
       }
 
       var newRowHtml1 = '<tr id="';
-      var newRowHtml2 = '"><td><form action="/updatecompleted/" method="post" role="form"><input type="checkbox" name="box_name" value="';
+      var newRowHtml2 = '" class="table-link"><td><form action="/updatecompleted/" method="post" role="form"><input type="checkbox" name="box_name" value="';
       var newRowHtml3 = '" class="completed-box"></form></td><td>';
       var newRowHtml4 = '</td><td><a href="/details/';
       var newRowHtml5 = '"><div class="pull-right"></div></a></td></tr>';
